1c3f14066751c9edd1e9d9b35dccb5921d1a2c79
Victoria Kee <victoriakee95@gmail.com>
2021-04-23 11:59:06 -0500

    Change value colors based on count

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,13 @@ btns.forEach(function(btn){
         } else if(styles.contains('increase')){
             count++;
         }
+        if(count<0){
+            value.style.color = "red";
+        } else if(count === 0){
+            value.style.color = "black";
+        } else if(count > 0){
+            value.style.color = "green";
+        }
         value.textContent = count;
     })
 });
