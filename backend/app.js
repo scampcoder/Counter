8a56eec238b9ac57edc91b16734d353ee154e94f
Victoria Kee <victoriakee95@gmail.com>
2021-04-23 11:58:48 -0500

    Write btn functionality

--- backend/app.js
+++ backend/app.js
@@ -5,3 +5,19 @@ let count = 0;
 const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
 
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        //testing event selection
+        //console.log(e.currentTarget.classList);
+        //grab event listener selection
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        } else if(styles.contains('reset')){
+            count = 0;
+        } else if(styles.contains('increase')){
+            count++;
+        }
+        value.textContent = count;
+    })
+});
